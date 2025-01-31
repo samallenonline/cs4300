53e6b5ac584c8e84327aa4b9f64d434bfd22e38a
Sam Allen <sallen21@uccs.edu>
2025-01-31 10:13:12 +0000

    Progress made on task 5

--- homework1/task5.py
+++ homework1/task5.py
@@ -1,7 +1,14 @@
 # TASK 5: Lists and Dictionaries 
 
 # create a list of your favorite books, including book titles and authors 
+# (I don't read books as often as I used to, but I love music so I will be making a list of some of my favorite albums instead)
+my_favorite_albums = ["E - Ecco2k", "Slide - George Clanton", "Warlord - Yung Lean", "Age Of - Oneohtrix Point Never", "The OOZ - King Krule"]
 
 # use list slicing to print the first three books in the list 
+print(my_favorite_albums[0:3])
 
 # create a dictionary that represents a basic student database, including student names and IDs
+student_database = {
+    "name": "Sam Allen",
+    "ID": "IDK!"
+}
--- homework1/test_task5.py
+++ homework1/test_task5.py
@@ -1,6 +1,11 @@
 # objective: test the task5.py program using pytest
 
 # import appropriate functions to be tested
-# from task5 import 
+import subprocess
 
-# test the output of each function 
+# test first part of task 5- printing the first 3 items in my_favorite_albums list
+def test_favorite_albums():
+    result = subprocess.run(["python3", "task5.py"], capture_output=True, text=True)
+    assert result.stdout == "['E - Ecco2k', 'Slide - George Clanton', 'Warlord - Yung Lean']\n"
+
+# test seconfd part of task 5

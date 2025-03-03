7d1c15075f236bcfc60bc06ddbaadae672139d53
Sam Allen <sallen21@uccs.edu>
2025-03-03 10:15:55 +0000

    Debugging

--- homework2/movie_theater_booking/bookings/urls.py
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # home page
-    path('movies/', views.movie_list, name='movie_list'),  # movie list HTML page
-    path('seats/', views.seat_booking, name='seat_booking'),    # seat booking HTML page
-    path('history/', views.booking_history, name='booking_history'),  # booking history HTML page
+    path('', views.home, name='home'),
+    path('movies/', views.movie_list, name='movie_list'),
+    path('seats/', views.seat_booking, name='seat_booking'),
+    path('history/', views.booking_history, name='booking_history'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -141,6 +141,6 @@ REST_FRAMEWORK = {
 }
 
 # Authentication settings
-LOGIN_URL = '/bookings/account/login'
+LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/bookings/movies/'
 LOGOUT_REDIRECT_URL = '/bookings/movies/'
